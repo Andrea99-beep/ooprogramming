e87a5eb55d52a02838661c859236f719b77c7d39
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2020-05-04 23:48:37 +0200

    update

--- code/IFTS/src/Basics/Fibonacci.java
+++ code/IFTS/src/Basics/Fibonacci.java
@@ -2,21 +2,15 @@ package Basics;
 
 public class Fibonacci {
 	public static void main(String[] args) {
-		int[] lastTwo = {0, 1};
+		int[] lastTwo = { 0, 1 };
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			int f = lastTwo[0] + lastTwo[1];
-			
+
 			lastTwo[0] = lastTwo[1];
 			lastTwo[1] = f;
-			
-			System.out.printf("[%d, %d] --> %d\n", 
-					lastTwo[0],
-					lastTwo[1],
-					f);
+
+			System.out.printf("[%d, %d] --> %d\n", lastTwo[0], lastTwo[1], f);
 		}
 	}
-
-
-
 }
